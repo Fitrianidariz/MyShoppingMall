a14ce71122e4bd3b7ef47ff89a0f849f8ce7fb31
fitrianidariz <i_love_him_id@yahoo.com>
2016-07-20 12:08:49 +0700

    tambah detaiProductActivity

--- app/src/main/java/com/example/multimatics/myshoppingmall/SampleData.java
+++ app/src/main/java/com/example/multimatics/myshoppingmall/SampleData.java
@@ -9,4 +9,10 @@ public class SampleData {
                 {"Lavender Milk Tea Bubble", "http://www.calaistea.com/ocms/images/our_menu_images/thumb-lavender-milk-tea-+-boba-1346380086.png", "Rp. 22.000"},
                 {"Lavender Milk Tea Pink Bubble", "http://www.calaistea.com/ocms/images/our_menu_images/thumb-lavender-milk-tea-+-pinkboba-1346380115.png", "Rp. 22.000"}
         };
+        public static String[] thumb = new String[]{
+                "http://www.calaistea.com/ocms/images/our_menu_images/thumb-cappuccino-1347609588.png",
+                "http://www.calaistea.com/ocms/images/our_menu_images/thumb-milk-latte-+-boba-1348456839.png",
+                "http://www.calaistea.com/ocms/images/our_menu_images/thumb-lavender-milk-tea-+-miniboba-1346380142.png",
+                "http://www.calaistea.com/ocms/images/our_menu_images/thumb-macha-milk-tea-+-regular-boba-1346380333.png"
+        };
 }
